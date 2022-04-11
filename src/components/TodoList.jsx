161f5666acfb8687b20f05b233d12fd314abf986
akifzsche <akif.tonmaister58@gmail.com>
2022-04-11 20:53:25 +0300

    toggle, destroy {confirm}

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggle, destroy } from '../todos/TodosSlice'
 //useSelector ile redux store'daki todos stateini alıyoruz
 function TodoList() {
     //store'daki todos stateini alıyoruz ve items'a atıyoruz
     //state'in altındaki todos isimli slice'ın items'ını alıyoruz.İtems oradaki obje.
     const items = useSelector(state => state.todos.items)
+    const dispatch = useDispatch()
+
+    const handleDestroy = (id) => {
+        if (window.confirm('Are you sure?')) {
+            dispatch(destroy(id))
+        }
+
+    }
+
     return (
         <ul className="todo-list">
-            <li>
-                <div className="view">
-                    <input className="toggle" type="checkbox" />
-                    <label>akifzsche</label>
-                    <button className="destroy"></button>
-                </div>
-            </li>
             {
                 items.map(item => (
                     <li key={item.id} className={item.completed ? 'completed' : ''}>
                         <div className="view">
-                            <input className="toggle" type="checkbox" />
+                            <input className="toggle" type="checkbox"
+                                onChange={() => dispatch(toggle({ id: item.id }))} checked={item.completed} />
                             <label>{item.title}</label>
-                            <button className="destroy"></button>
+                            <button className="destroy" onClick={() => handleDestroy(item.id)}></button>
                         </div>
                     </li>
                 ))
--- src/todos/TodosSlice.jsx
+++ src/todos/TodosSlice.jsx
@@ -19,9 +19,23 @@ export const TodosSlice = createSlice({
         addTodo: (state, action) => {
             //addTodo'nun amacı state'in altındaki items objesini pushlamak
             state.items.push(action.payload);
-        }
-    }
-})
+        },
+        toggle: (state, action) => {
+            //toggle'nun amacı state'in altındaki items objesini completed'ını true/false yapmak
+            const { id } = action.payload;
+
+            const item = state.items.find(item => item.id === id);
+            item.completed = !item.completed;
+        },
+        destroy: (state, action) => {
+            //destroy'nun amacı state'in altındaki items objesini silmek
+            const id = action.payload;
+            //seçilen id harici id'li tüm elemanları toplamak istiyorum. item.id denk değilse id filtre içine eklensin
+            const filtered = state.items.filter(item => item.id !== id);
+            state.items = filtered;
+        },
+    },
+});
 //reducerları export ediyoruz
-export const { addTodo } = TodosSlice.actions;
+export const { addTodo, toggle, destroy } = TodosSlice.actions;
 export default TodosSlice.reducer;
