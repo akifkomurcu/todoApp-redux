97fbd287f04870995a6d66e979d6144b20919240
akifzsche <akif.tonmaister58@gmail.com>
2022-04-12 21:12:00 +0300

    uygulama bitti

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
     return (
         <footer className="info">
-            <p>Click to edit a todo</p>
-            <p>Created by <a href="akifzsche.github.io">Akif Komurcu</a></p>
+
+            <p>Created by <a href="https://akifzsche.github.io/" target={"_blank"}><strong>Akif Komurcu</strong></a></p>
 
         </footer>
     )
